964b2745e8cda8ec2971bf8a640dc3684683b3ba
mliberson <mliberso@usc.edu>
2022-04-07 11:24:12 -0700

    created lcd header file

--- lcd/lcd-test.c
+++ lcd/lcd-test.c
@@ -17,14 +17,8 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
-
-void lcd_init(void);
-void lcd_moveto(unsigned char, unsigned char);
-void lcd_stringout(char *);
-
-void sci_init(void);
-void sci_out(char);
-void sci_outs(char *);
+#include "serial.h"
+#include "lcd.h"
 
 char str1[] = "1234567890";
 char str2[] = ">> USC EE459L <<";
@@ -37,7 +31,7 @@ char str4[] = "-- April 11, 2011 --";
 
 int main(void) {
 
-    sci_init();                 // Initialize the SCI port
+    serial_init(MYUBRR);                 // Initialize the SCI port
     
     lcd_init();                 // Initialize the LCD
 
@@ -54,95 +48,4 @@ int main(void) {
     }
 
     return 0;   /* never reached */
-}
-
-/*
-  lcd_init - Initialize the LCD
-*/
-void lcd_init()
-{
-    _delay_ms(250);             // Wait 500msec for the LCD to start up
-    _delay_ms(250);
-    sci_out(0xfe);              // Clear the screen
-    sci_out(0x51);
-}
-
-/*
-  moveto - Move the cursor to the row and column given by the arguments.
-  Row is 0 or 1, column is 0 - 15.
-*/
-void lcd_moveto(unsigned char row, unsigned char col)
-{
-    char ch;
-    switch(row) 
-    {
-      case 0:
-        ch = 0x00;
-        break;
-      case 1:
-        ch = 0x40;
-        break;
-      case 2:
-        ch = 0x14;
-        break;
-      case 3:
-        ch = 0x54;
-        break;
-    }
-    ch += col;
-    
-    sci_out(0xfe);              // Set the cursor position
-    sci_out(0x45);
-    sci_out(ch);
-    _delay_ms(1000);             // Wait 500msec for the LCD to start up
-}
-
-
-/*
-  lcd_stringout - Print the contents of the character string "str"
-  at the current cursor position.
-*/
-void lcd_stringout(char *str)
-{
-    sci_outs(str);              // Output the string
-}
-
-/* ----------------------------------------------------------------------- */
-
-/*
-  sci_init - Initialize the SCI port
-*/
-void sci_init(void) {
-    UBRR0 = MYUBRR;          // Set baud rate
-    UCSR0B |= (1 << TXEN0);  // Turn on transmitter
-    UCSR0B |= (1 << RXEN0 ); // Turn on receiver
-    UCSR0C = (3 << UCSZ00);  // Set for asynchronous operation, no parity, 
-                             // one stop bit, 8 data bits
-}
-
-/*
-  sci_out - Output a byte to SCI port
-*/
-void sci_out(char ch)
-{
-    while ( (UCSR0A & (1<<UDRE0)) == 0);
-    UDR0 = ch;
-}
-
-/*
-  sci_outs - Print the contents of the character string "s" out the SCI
-  port. The string must be terminated by a zero byte.
-*/
-void sci_outs(char *s)
-{
-    char ch;
-
-    while ((ch = *s++) != '\0')
-        sci_out(ch);
-}
-
-char serial_in ()
-{
-while ( !( UCSR0A & (1 << RXC0 )) );
-return UDR0 ;
 }
